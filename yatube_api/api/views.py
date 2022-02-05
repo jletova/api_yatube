5316c4442a4ef61d87892b2d81a6f0529ff60c28
Julia Letova <julialikh@gmail.com>
2022-02-05 20:28:41 +0100

    My project for review

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,8 +1,4 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.shortcuts import get_object_or_404
-
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
