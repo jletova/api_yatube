27ac380b64de982a659d5cad5698bb13401667f2
Julia Letova <julialikh@gmail.com>
2022-02-06 16:07:57 +0100

    My project for review

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -5,5 +5,5 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
     messgage = "Редактирование чужого контента запрещено"
 
     def has_object_permission(self, request, view, obj):
-        return (request.method in permissions.SAFE_METHODS or
-                obj.author == request.user)
+        return (request.method in permissions.SAFE_METHODS
+                or obj.author == request.user)
